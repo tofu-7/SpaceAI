9e801aff757c77c8de6b95c00230faf89959f728
Scott3.0 <bremmerscottw@gmail.com>
2020-10-07 10:30:46 -0700

    forgot a thing

--- Assets/Scripts/Component Scripts/Core.cs
+++ Assets/Scripts/Component Scripts/Core.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CoreTraits
+public int enum CoreTraits
 {
     offspringCount = 1,
     offspringMutationChance = 15, //0 to 100
@@ -18,8 +18,8 @@ public class Core : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+
+    } 
 
     // Update is called once per frame
     void Update()
