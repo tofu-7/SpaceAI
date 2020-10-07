c68d75d033fa5b23c70438a6dabb94d223ab6604
Scott3.0 <bremmerscottw@gmail.com>
2020-10-07 10:15:08 -0700

    Implemented dinos scrips and started work on component variables

--- Assets/Scripts/Component Scripts/Core.cs
+++ Assets/Scripts/Component Scripts/Core.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum CoreTraits
+{
+    offspringCount = 1,
+    offspringMutationChance = 0.15f, //0 to 1
+    sensingRange =
+    mass = 1,
+    initStorageCap = 10
+}
+
 public class Core : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
