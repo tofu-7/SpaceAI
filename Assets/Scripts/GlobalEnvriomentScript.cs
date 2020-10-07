f3dbe84dac2dcb8fd661190068b22cfa1ef64245
Daniel <danielmholland1@gmail.com>
2020-10-07 10:14:43 -0700

    Update GlobalEnvriomentScript.cs

--- Assets/Scripts/GlobalEnvriomentScript.cs
+++ Assets/Scripts/GlobalEnvriomentScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GlobalEnvriomentScript : MonoBehaviour
 {
+
+    public static float TileSize = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
