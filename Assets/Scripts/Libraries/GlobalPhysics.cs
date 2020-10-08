5493c835b4fbd3d6fc7bbb60712f267e781a0ebb
Alex Giger <a13xgiger@gmail.com>
2020-10-08 15:23:06 -0700

    Added physics script

---
+++ Assets/Scripts/Libraries/GlobalPhysics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalPhysics : MonoBehaviour
+{
+    float speed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+}
