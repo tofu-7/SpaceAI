5f8cf7c8773fe8b29a44f2dc84955c531ea360aa
Daniel <danielmholland1@gmail.com>
2020-10-16 15:39:09 -0700

    made it so that thingmadoodles take time or something

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -13,6 +13,7 @@ public class ResourceGenerator : MonoBehaviour
     public static int ResourceCap = 50; //stream This Guitar by Fraxiom cus they are poggers
     public static int mouthCap = 10; //omnom
     public static int thrusterCap = 10; //woosh
+    public static float spawnDelay = 1f; //time is fun -_-
 
     public bool renewableResources = true; //if only we had this setting irl
     public bool renewableComponents = false; //TODO Implement this
@@ -21,6 +22,8 @@ public class ResourceGenerator : MonoBehaviour
     public Transform resource; 
     public Transform mouth;
     public Transform thruster; //TODO is there a better way of doing this
+
+    private int rSpawnCount = 0;
 
     void Start() //initial generation of resources runs on sim start
     {
@@ -45,10 +48,15 @@ public class ResourceGenerator : MonoBehaviour
     {
         if(renewableResources) //does thing if turned on
         {
-            for (int i = 0; i < ResourceCap-tagCount("Resource"); i++) //makes new resources to cap
+            /*for (int i = 0; i < ResourceCap-tagCount("Resource"); i++) //makes new resources to cap
             {
-                Transform newR = Instantiate(resource).GetComponent<Transform>();
-                newR.localPosition = new Vector3(Random.Range(0f, GlobalEnvironmentLib.xBound), Random.Range(0f, GlobalEnvironmentLib.yBound), 0);
+                StartCoroutine(spawn(resource, spawnDelay));
+            }*/
+
+            if (ResourceCap > tagCount("Resource") + rSpawnCount)
+            {
+                StartCoroutine(spawn(resource, spawnDelay));
+                rSpawnCount++;
             }
         }
 
@@ -61,7 +69,7 @@ public class ResourceGenerator : MonoBehaviour
                 newR.localPosition = new Vector3(Random.Range(0f, GlobalEnvironmentLib.xBound), Random.Range(0f, GlobalEnvironmentLib.yBound), 0);
             }
 
-            for (int i = 0; i < mouthCap - tagCount("Mouth"); i++) //makes new thrusters to cap
+            for (int i = 0; i < mouthCap - tagCount("Mouth"); i++) //makes new mouths to cap
             {
                 Transform newR = Instantiate(mouth).GetComponent<Transform>();
                 newR.localPosition = new Vector3(Random.Range(0f, GlobalEnvironmentLib.xBound), Random.Range(0f, GlobalEnvironmentLib.yBound), 0);
@@ -85,5 +93,15 @@ public class ResourceGenerator : MonoBehaviour
 
         return arraycount.Length; //kinda dissapointed as to how easy this was compared to my previous jank
        
+    }
+
+    IEnumerator spawn(Transform prefab, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        Transform newR = Instantiate(prefab).GetComponent<Transform>();
+        newR.localPosition = new Vector3(Random.Range(0f, GlobalEnvironmentLib.xBound), Random.Range(0f, GlobalEnvironmentLib.yBound), 0);
+
+        rSpawnCount--;
     }
 }
