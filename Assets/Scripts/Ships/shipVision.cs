5e6add795d7e8259ffe9222f3c366f61444229ae
Daniel <danielmholland1@gmail.com>
2020-10-23 14:46:19 -0700

    started the thing

---
+++ Assets/Scripts/Ships/shipVision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipVision : MonoBehaviour
+{
+
+    float visionRange = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Collider2D nearestResource(Transform ship)
+    {
+        Collider2D[] things = Physics2D.OverlapCircleAll();
+    }
+}
