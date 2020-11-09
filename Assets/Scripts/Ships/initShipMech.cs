1b4e1a0a6f5f3ebe0509ad5ed2126e9802d3c863
Scott3-0 <bremmerscottw@gmail.com>
2020-11-09 09:56:47 -0800

    Added new file

--- Assets/Scripts/Ships/initShipMech.cs
+++ Assets/Scripts/Ships/initShipMech.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class initShipV3 : MonoBehaviour
+public class initShipMech : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
