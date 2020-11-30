6efda503980a12585d49e6f8022e5620ac439a5a
Daniel <danielmholland1@gmail.com>
2020-11-30 10:29:48 -0800

    my attempt at stealing code from the internet did not work ima just code it myself later

---
+++ Assets/Scripts/ResourceDistSort.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ResourceDistSort : MonoBehaviour
+{
+
+    private GameObject[] resources;
+    public GameObject nearResource;
+
+    void Update()
+    {
+        nearResource = NearResource();
+    }
+
+    GameObject NearResource()
+    {
+        resources = GameObject.FindGameObjectsWithTag("resources");
+        resources = resources.OrderBy( x => Vector2.Distance(this.transform.position, x.transform.position)).ToArray();
+        return resources[0];
+    }
+}
