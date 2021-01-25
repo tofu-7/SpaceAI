ce991b8ae7cf0b7ee6f23e662ed6eb3ecfeb69ae
Alex Giger <57809929+giiger@users.noreply.github.com>
2021-01-25 14:04:17 -0800

    off screen arrow

--- Assets/Scripts/offScreenArrow.cs
+++ Assets/Scripts/offScreenArrow.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class offScreenArrow : MonoBehaviour
 {
+  //Our object
     public GameObject core;
 
     void Update()
     {
-        Vector3 screenCenter = new Vector3(Screen.width, Screen.height,0) /2;
-        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(core.transform.position);
+        if(core.GetComponent<Renderer>().isVisible == false)
+        {
+          //Getting the screen center so I can use the angle between the center of the screen and the position of the ship to get the rotation of the arrow
+            Vector3 screenCenter = new Vector3(Screen.width, Screen.height,0) /2;
 
-        Vector3 dir = (targetPositionScreenPoint - screenCenter).normalized;
-        float angle = Mathf.Atan2(dir.y,dir.x);
+          //This is used to get the position of the ship
+            Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(core.transform.position);
 
-        transform.position = screenCenter + new Vector3(Mathf.Cos(angle) * screenCenter.x, Mathf.Sin(angle) * screenCenter.y, 0);
-        transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+          //Finding the direction the ship is relative to the screen center, and we use normalized because we want it to be less than 1 so we can convert it into degrees
+            Vector3 dir = (targetPositionScreenPoint - screenCenter).normalized;
+
+          //Getting the angle the arrow should point
+            float angle = Mathf.Atan2(dir.y,dir.x);
+
+          //Setting the position of the arrow to match the position of the target relative to the center of the screen
+            transform.position = screenCenter + new Vector3(Mathf.Cos(angle) * screenCenter.x, Mathf.Sin(angle) * screenCenter.y, 0);
+
+          //Setting the angle of the arrow to rotate around the center of the screen and point towards the target
+            transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+        }
+        else
+        {
+          transform.position = new Vector3(3000,3000,0);
+        }
     }
 }
